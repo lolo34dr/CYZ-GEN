344f806bd53bc7f3eaaf77b1ec485e06b8d03aac
CYZ <165677542+lolo34dr@users.noreply.github.com>
2025-05-07 17:36:06 +0200

    Add files via upload

---
+++ digi.bat
@@ -0,0 +1,38 @@
+@echo off
+title Infos - Ce que peut faire un Digispark Attiny85
+color 0a
+
+echo =====================================================
+echo         DIGISPARK ATTINY85 - POSSIBILITES
+echo =====================================================
+echo.
+echo [1] Simulation de clavier (HID)
+echo - Le Digispark peut se faire passer pour un clavier.
+echo - Il peut taper des commandes automatiquement sur un PC.
+echo.
+
+echo [2] Automatisation de tâches
+echo - Il peut ouvrir des programmes (ex: Bloc-Notes, CMD).
+echo - Il peut naviguer vers des sites web.
+echo - Il peut exécuter des scripts téléchargés automatiquement.
+echo.
+
+echo [3] Création d'utilisateurs ou changements système
+echo - Avec des droits admin, il peut créer un utilisateur.
+echo - Il peut modifier des paramètres Windows rapidement.
+echo.
+
+echo [4] Attaques types Rubber Ducky
+echo - Il est utilisé pour des démonstrations de tests de sécurité.
+echo - Il peut simuler une attaque en quelques secondes après branchement.
+echo.
+
+echo [5] Utilisation éducative
+echo - Parfait pour apprendre la sécurité offensive.
+echo - Peut être utilisé dans les formations en cybersécurité.
+echo.
+
+echo =====================================================
+echo     bat par Louis Marley
+echo =====================================================
+pause
